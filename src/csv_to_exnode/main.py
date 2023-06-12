c1f5e87ea8594f5cfee8c697bc23e17f4cadaca3
elias-soltani <egha355@aucklanduni.ac.nz>
2023-06-12 14:02:36 +1200

    minor change

--- src/csv_to_exnode/main.py
+++ src/csv_to_exnode/main.py
@@ -6,7 +6,6 @@ import argparse
 from csv_to_exelem.csv_to_exelem import CsvToExf
 
 if __name__ == "__main__":
-    # inputFile = r"C:\Users\egha355\Desktop\work_related\skin_point_cloud\torso_data_alex.csv"
     ap = argparse.ArgumentParser(description='Convert a csv file to an exnode file.')
     ap.add_argument('-i', '--input', help='Input csv file', required=True)
     args = ap.parse_args()
